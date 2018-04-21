b64f37de395e0b5451b673d0028d6b10b5519254
Hayoung Jeon <wjsgkdud9000@naver.com>
2018-04-21 20:35:49 +0900

    Keyword Test

---
+++ Chapter02_01/CMyClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter02_01
+{
+    class CMyClass
+    {
+        public int theStudentCount;
+        public string theName;
+        public int theGrade;
+
+        public CMyClass()
+        {
+            theStudentCount = 20;
+            theName = "NoName";
+            theGrade = 0;
+        }
+        public CMyClass(int aGrade)
+        {
+            theStudentCount = 20;
+            theName = "NoName";
+            theGrade = aGrade;
+        }
+        public CMyClass(int aGrade,string aName) : this()
+        {
+            theName = aName;
+            theGrade = aGrade;
+        }
+        public string GetName()
+        {
+            string res = string.Format("{0}학년,{1},{2}명", theGrade, theName, theStudentCount);
+            return (res);
+        }
+        public void Increase(int aValue)
+        {
+            aValue++;
+        }
+        public void Increase(ref int aValue)
+        {
+            aValue++;
+        }
+        public void MakeValue(out int aValue)
+        {
+            aValue = 19;
+        }
+    }
+}
---
+++ Chapter02_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CMyClass tmpMC1 = new CMyClass();
+            Console.WriteLine(tmpMC1.GetName());
+
+            Console.WriteLine(new CMyClass(1).GetName());
+            Console.WriteLine(new CMyClass(3,"AAA").GetName());
+
+            int a = 5;
+            int b = 5;
+            tmpMC1.Increase(a);
+            tmpMC1.Increase(ref b); //ref 초기화 필수
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            int c;
+            tmpMC1.MakeValue(out c); //out은 초기화 안해도 됨
+            Console.WriteLine(c); //대신 함수에서 값을 넣는 것이 필수
+        }
+    }
+}
